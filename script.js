8a5897abe82d41da566b90b1dcb6734c4dc3af53
official_brodnikov <brodnikov111@gmail.com>
2020-02-03 17:58:21 +0300

    Add css

---
+++ script.js
@@ -0,0 +1,13 @@
+var text = document.getElementById("1");
+var button = document.getElementById("2");
+var list = document.getElementById("3");
+
+button.addEventListener("click", function() 
+{
+	if (text.value.length > 0)
+	{
+		let elem = document.createElement('li');
+	  	elem.innerHTML = text.value;
+		list.append(elem);
+	}
+});
