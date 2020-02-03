a8b2137c3674d918ee63d621f74fa84581683e0a
official_brodnikov <brodnikov111@gmail.com>
2020-02-03 20:11:54 +0300

    Add operation of remove

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-var text = document.getElementById("1");
-var button = document.getElementById("2");
-var list = document.getElementById("3");
+var text = document.getElementById("inputBox");
+var button = document.getElementById("addButton");
+var list = document.getElementById("listElem");
+var i = 0;
 
-button.addEventListener("click", function() 
+
+button.addEventListener("click", function(e) 
 {
 	if (text.value.length > 0)
 	{
 		let elem = document.createElement('li');
-	  	elem.innerHTML = text.value;
+	  	i=i+1;
+		elem.id = "elem" + i;
+		elem.innerHTML = text.value;
 		list.append(elem);
 		text.value="";
-	}
+		var deleteButton = document.createElement('input');
+		deleteButton.type = "button";
+		deleteButton.id = "deleteButton" + i;
+		deleteButton.value = "X";
+		deleteButton.style.cssText = 'color: red;  background-color: transparent; border: medium none; font-size: 18px;';
+		elem.appendChild (deleteButton);
+		deleteButton.addEventListener("click", function()
+		{
+			var str = this.id;
+			var deleteElem = document.getElementById("elem" + str.replace(/[^\d]/g, ''));
+			deleteElem.remove();
+			this.remove();
+		});
+	} 
 });
+
+
+
