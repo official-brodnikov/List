723c33ccccef72b9d4ac62a3c7d75a5184f06216
official_brodnikov <brodnikov111@gmail.com>
2020-02-04 19:15:13 +0300

    Add operaion of sort list

--- script.js
+++ script.js
@@ -2,122 +2,148 @@ var text = document.getElementById("inputBox");
 var button = document.getElementById("addButton");
 var list = document.getElementById("listElem");
 var i = 0;
+var radio1 = document.getElementById("radio1");
 
 document.querySelector('ul').addEventListener("click", function(e) {
-  const deleteButton = e.target.closest('.delete');
-  if (!deleteButton) 
-  {
-	const editButton = e.target.closest('.edit');
-	if (!editButton) 
+	//удаление
+	let deleteButton = e.target.closest('.delete');
+	if (deleteButton) 
 	{
-		const cancelButton = e.target.closest('.cancel');
-		const okButton = e.target.closest('.ok');
-		if (!okButton && !cancelButton) 
-		{
-			return;
-		}
-		let elem = okButton ? okButton.parentElement : cancelButton.parentElement;
-		let newInputButton = elem.children[0].value;
-		elem.removeChild(elem.firstChild);
-		elem.children[1].style.display = "inline";
-		elem.children[2].style.display = "inline";
-		elem.children[3].style.display = "none";
-		elem.children[4].style.display = "none";
-		if (okButton) 
-		{
-			if (newInputButton != "")
-			{
-				elem.children[0].text = newInputButton;
-			}
-		}
-		elem.children[0].style.display = "inline";
+		deleteButton.parentElement.remove();
 		return;
 	}
 	
-	let elem = editbutton.parentElement;
-	elem.children[0].style.display = "none";
-	elem.children[1].style.display = "none";
-	elem.children[2].style.display = "none";
-	elem.children[3].style.display = "inline";
-	elem.children[4].style.display = "inline";	
+	//изменение
+	const editButton = e.target.closest('.edit');
+	if (!editButton) {
+		return;
+	}
+	let listElements = editButton.parentElement;
+	listElements.children[1].style.display = "none";
+	listElements.children[2].style.display = "none";
+	listElements.children[3].style.display = "none";
+	listElements.children[4].style.display = "inline";
+	listElements.children[5].style.display = "inline";	
 	let input = document.createElement("input");
 	input.id = "edit-input"
 	input.type = "text";
-	input.value = elem.children[0].text;
-	elem.prepend(input);
-	return;
-  }
-  deleteButton.parentElement.remove();
+	input.value = listElements.children[1].text;
+	listElements.insertBefore(input, listElements.children[1]);
+});
+
+document.querySelector('ul').addEventListener("click", function(e) {
+	const cancelButton = e.target.closest('.cancel');
+	const okButton = e.target.closest('.ok');
+	if (!okButton && !cancelButton) {
+		return;
+	}
+	
+	let listElements = okButton ? okButton.parentElement : cancelButton.parentElement;
+	let newText = listElements.children[1].value;
+	
+	listElements.removeChild(listElements.children[1]);
+	listElements.children[2].style.display = "inline";
+	listElements.children[3].style.display = "inline";
+	listElements.children[4].style.display = "none";
+	listElements.children[5].style.display = "none";
+	
+	if (okButton) {
+		if (newText != "")
+			listElements.children[1].text = newText;	
+	}
+	listElements.children[1].style.display = "inline";
+});
+
+
+
+
+
+function Sort(order, index) {
+	var arrayElem = new Array();
+	var arrayIndex = new Array();
+	var listElem=document.getElementsByTagName('li');
+	
+	for(var k=0;k<listElem.length;k++) 
+	{
+		arrayElem[k]=listElem[k].children[1].text;
+		arrayIndex[k]=listElem[k].children[0].text;
+	}
+	
+	if (index)
+		bubbleSort(arrayElem, arrayIndex);
+	else
+		bubbleSort(arrayIndex, arrayElem);
+	
+	if (!order)
+	{
+		arrayElem.reverse();
+		arrayIndex.reverse();
+	}
+	
+	for(var k=0;k<arrayElem.length;k++) 
+	{
+		listElem[k].children[1].text=arrayElem[k];
+		listElem[k].children[0].text=arrayIndex[k];
+	}
+}
+
+function bubbleSort(arrayElem, arrayIndex) {
+    for (var k = 0, endI = arrayElem.length - 1; k < endI; k++) {
+        for (var j = 0, endJ = endI - k; j < endJ; j++) {
+            if (arrayIndex[j] > arrayIndex[j + 1]) {
+                var swapElem = arrayElem[j];
+				var swapIndex = arrayIndex[j];
+				arrayIndex[j] = arrayIndex[j + 1];
+                arrayElem[j] = arrayElem[j + 1];
+                arrayElem[j + 1] = swapElem;
+				arrayIndex[j + 1] = swapIndex;
+            }
+        }
+    }
+}
+	
+document.getElementById("radioSortABC").addEventListener('click', function (event) {
+    if (event.target && event.target.matches("input[type='radio']")) {
+		Sort(true, false);
+    }
+});
+
+document.getElementById("radioSortNotABC").addEventListener('click', function (event) {
+    if (event.target && event.target.matches("input[type='radio']")) {
+        Sort(false, false);
+    }
+});
+
+document.getElementById("radioSortIndex").addEventListener('click', function (event) {
+    if (event.target && event.target.matches("input[type='radio']")) {
+        text.value = "1";
+		Sort(true, true);
+    }
+});
+
+
+document.getElementById("radioSortNotIndex").addEventListener('click', function (event) {
+    if (event.target && event.target.matches("input[type='radio']")) {
+        Sort(false, true);
+    }
 });
 
 button.addEventListener("click", function(e) 
 {
 	if (text.value.length > 0)
 	{
+		i = i + 1;
 		let elem = document.createElement("li");
+		let index = "<a>" + i + ". " + "</a>";
 		let value = "<a>" + text.value + "</a>";
 		let deleteButton = "<button class=\"delete\" id=\"deletebutton\">X</button>";
 		let editButton = "<button class=\"edit\" id=\"editbutton\">Изменить</button>";
 		let okButton = "<button class=\"ok\" id=\"okbutton\">Ок</button>";
 		let cancelButton = "<button class=\"cancel\" id=\"cancelbutton\">Отменить</button>";
-		elem.innerHTML = value + deleteButton + editButton + okButton + cancelButton;
+		elem.innerHTML = index + value + deleteButton + editButton + okButton + cancelButton;
 		list.append(elem);
 		text.value = "";
-		elem.children[3].style.display = "none";
-		elem.children[4].style.display = "none";	
+		elem.children[4].style.display = "none";
+		elem.children[5].style.display = "none";	
 	}
-	
-		
-		/*let elem = document.createElement('li');
-	  	i=i+1;
-		elem.id = "elem" + i;
-		let value = "<a>" + text.value + "</a>";
-		elem.innerHTML = value;
-		list.append(elem);
-		text.value="";
-		
-		var deleteButton = document.createElement('input');
-		deleteButton.type = "button";
-		deleteButton.id = "deleteButton" + i;
-		deleteButton.value = "X";
-		deleteButton.style.cssText = 'color: red;  background-color: transparent; border: medium none; font-size: 18px;';
-		elem.appendChild (deleteButton);
-		
-		var newInputButton = document.createElement('text-input');
-		newInputButton.value = "";
-		elem.appendChild (newInputButton);
-		newInputButton.style.display = 'none';
-		
-		var editButton = document.createElement('input');
-		editButton.type = "button";
-		editButton.id = "editButton" + i;
-		editButton.value = "изменить";
-		editButton.style.cssText = 'color: #41d109;  background-color: transparent; border: medium none; font-size: 18px;';
-		elem.appendChild (editButton);
-		
-		var cancelButton = document.createElement('input');
-		cancelButton.type = "button";
-		cancelButton.value = "отменить";
-		cancelButton.style.cssText = 'color: red;  background-color: transparent; border: medium none; font-size: 18px;';
-		elem.appendChild (cancelButton);
-		cancelButton.style.display = 'none';
-		
-		var okButton = document.createElement('input');
-		okButton.type = "button";
-		okButton.value = "ok";
-		okButton.style.cssText = 'color: #41d109;  background-color: transparent; border: medium none; font-size: 18px;';
-		elem.appendChild (okButton);
-		okButton.style.display = 'none';
-		*/
-		
-		/*editButton.addEventListener("click", function(e)
-		{
-			var str = this.id;
-			var deleteElem = document.getElementById("elem" + str.replace(/[^\d]/g, ''));
-			cancelButton.style.display = 'inline';
-			okButton.style.display = 'inline';
-			newInputButton.style.display = 'inline';
-			editButton.style.display = 'none';
-			deleteButton.style.display = 'none';
-		});*/ 
 });
